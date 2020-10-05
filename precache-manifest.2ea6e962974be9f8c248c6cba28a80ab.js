03a1a588f8059a795ede2848902c5f20f14045b5
Daniel Pereira <danielpereirasuporte@gmail.com>
2020-10-05 20:00:36 -0300

    first commit

---
+++ precache-manifest.2ea6e962974be9f8c248c6cba28a80ab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1fafbd7359e63035a1dcdf6d872fda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a05ce96109d7ce67901",
+    "url": "./static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "411672e2aa3219919c92",
+    "url": "./static/css/main.056b067b.chunk.css"
+  },
+  {
+    "revision": "4a05ce96109d7ce67901",
+    "url": "./static/js/2.309f2b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.309f2b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411672e2aa3219919c92",
+    "url": "./static/js/main.dab06fcc.chunk.js"
+  },
+  {
+    "revision": "703e830397ad45d6ed8b",
+    "url": "./static/js/runtime-main.7501ba19.js"
+  },
+  {
+    "revision": "4395b29d9210bd75051ebce1e66e186d",
+    "url": "./static/media/AirbnbCerealBlack.4395b29d.ttf"
+  },
+  {
+    "revision": "c94c29feb80b02db176028d16fc634d2",
+    "url": "./static/media/AirbnbCerealBold.c94c29fe.ttf"
+  },
+  {
+    "revision": "86b15ac031e0507ad790afafd9adf7f7",
+    "url": "./static/media/AirbnbCerealBook.86b15ac0.ttf"
+  },
+  {
+    "revision": "0a0cbe1eca15f71b7187b38b1173eb98",
+    "url": "./static/media/AirbnbCerealExtraBold.0a0cbe1e.ttf"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "./static/media/AirbnbCerealLight.1f9c7d32.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "./static/media/AirbnbCerealMedium.d9fb3054.ttf"
+  }
+]);
